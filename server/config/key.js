e6af8477b987489b5334ab71883b39da675b1651
jinhomun <answlsgh95@gmail.com>
2024-01-08 09:56:20 +0900

    build

--- server/config/key.js
+++
@@ -1,7 +0,0 @@
-if (process.env.NODE_ENV === "production") {
-    // 배포 상태
-    module.exports = require("./production.js");
-} else {
-    // 개발 상태
-    module.exports = require("./dev.js");
-}
--- server/config/production.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI: process.env.MONGO_URI,
-    access_key: process.env.S3_KEY,
-    secret_key: process.env.S3_SECRET,
-}
