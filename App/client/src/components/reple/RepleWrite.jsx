4208a37c0dcd45b01f97ca3d4fd4acfe132a5270
jinhomun <answlsgh95@gmail.com>
2024-01-08 17:36:50 +0900

    댓글 업로드 될때 gsap

--- App/client/src/components/reple/RepleWrite.jsx
+++ App/client/src/components/reple/RepleWrite.jsx
@@ -1,72 +1,67 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
+// import { useSelector } from 'react-redux';
 import axios from 'axios';
 
 const RepleWrite = () => {
-    const [reple, setReple] = useState('');
-    const [displayName, setDisplayName] = useState('');
-    const [password, setPassword] = useState('');
+    const [reple, setReple] = useState("");
+    const [displayName, setDisplayName] = useState("");
+    const [password, setPassword] = useState("")
+    // const user = useSelector((state) => state.user);
 
-    const SubmitHandler = async (e) => {
+    const SubmitHandler = (e) => {
         e.preventDefault();
 
         // 비밀번호 길이 검증
-        if (password.length !== 4) {
-            return alert('비밀번호는 4자리로 입력해주세요.');
+        if (password.length < 4 || password.length > 4) {
+            return alert("비밀번호는 4자리로 입력해주세요.");
         }
 
         if (!reple) {
-            return alert('댓글 내용을 채워주세요!!!');
+            return alert("댓글 내용을 채워주세요!!!");
         }
 
-        try {
-            const response = await axios.post('/api/reple/submit', {
-                reple: reple,
-                displayName: displayName,
-                password: password,
-            });
+        let body = {
+            reple: reple,
+            displayName: displayName,
+            password: password
+            // uid: user.uid,
+            // postId: props.postId
+        }
 
+        axios.post("/api/reple/submit", body).then((response) => {
             if (response.data.success) {
-                alert('댓글 작성이 성공하였습니다.');
-                // 여기에서 페이지 리로드를 하지 않고, 상태를 업데이트할 수 있습니다.
+                alert("댓글 작성이 성공하였습니다.");
+                window.location.reload();
             } else {
-                alert('댓글 작성이 실패했습니다.');
+                alert("댓글 작성이 실패했습니다.");
             }
-        } catch (error) {
-            console.error('댓글 제출 중 오류:', error);
-            alert('댓글 작성 중 오류가 발생했습니다.');
-        }
-    };
+        })
+    }
 
     return (
-        <form onSubmit={SubmitHandler}>
+        <>
             <input
                 className="name-input"
                 placeholder="Your Name"
                 value={displayName}
-                onChange={(e) => {
-                    setDisplayName(e.currentTarget.value);
-                }}
+                onChange={(e) => { setDisplayName(e.currentTarget.value) }}
             />
             <textarea
                 className="comment-input"
                 placeholder="Input Comment"
                 value={reple}
-                onChange={(e) => {
-                    setReple(e.currentTarget.value);
-                }}
+                onChange={(e) => { setReple(e.currentTarget.value) }}
             ></textarea>
             <input
                 className="name-input"
                 placeholder="Password(4자리)"
                 type="password"
                 value={password}
-                onChange={(e) => {
-                    setPassword(e.currentTarget.value);
-                }}
+                onChange={(e) => { setPassword(e.currentTarget.value) }}
             />
-            <button className="submit" type="submit">등록</button>
-        </form>
+            <button className="submit" onClick={(e) => { SubmitHandler(e) }}>등록</button>
+        </>
     );
-};
+}
 
-export default RepleWrite;
+export default RepleWrite
--- App/client/src/page/Contact.jsx
+++ App/client/src/page/Contact.jsx
@@ -1,229 +1,172 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 import gsap from 'gsap';
-import RepleList from '../components/reple/RepleList'
-import RepleWrite from '../components/reple/RepleWrite'
+import RepleList from '../components/reple/RepleList';
+import RepleWrite from '../components/reple/RepleWrite';
 
-import insta from '../assets/img/insta.svg'
-import github from '../assets/img/github.svg'
-import mail from '../assets/img/mail.svg'
-import comment from '../assets/img/comment.svg'
-import arrow from '../assets/img/Arrow11.svg'
+import insta from '../assets/img/insta.svg';
+import github from '../assets/img/github.svg';
+import mail from '../assets/img/mail.svg';
+import comment from '../assets/img/comment.svg';
+import arrow from '../assets/img/Arrow11.svg';
 
-import "../assets/scss/section/_contact.scss"
+import '../assets/scss/section/_contact.scss';
 
 const Contact = () => {
     useEffect(() => {
         const hideScrollbar = () => {
-            const isBgAtTop = gsap.getProperty(".item.i5 .item__bg", "height") === "100%";
-            const itemInner = document.querySelector(".item.i5 .item__inner");
+            const isBgAtTop = gsap.getProperty('.item.i5 .item__bg', 'height') === '100%';
+            const itemInner = document.querySelector('.item.i5 .item__inner');
 
-            // item__inner의 클래스를 높이에 따라 토글합니다.
             if (itemInner) {
                 itemInner.classList.toggle('scrollable', !isBgAtTop);
             }
         };
 
-        gsap.to(".item.i5 .item__bg", {
-            height: "0%",
-            ease: "power3.inOut",
+        gsap.to('.item.i5 .item__bg', {
+            height: '0%',
+            ease: 'power3.inOut',
             delay: 0.6,
             onComplete: hideScrollbar,
         });
 
-        gsap.set(".item.i1 .item__inner", {
-            opacity: 0,
-            y: 30
-        });
-        gsap.set(".item.i5 .item__inner", {
-            opacity: 0,
-            y: 30
-        });
-        gsap.set(".item.i7 .item__inner", {
-            opacity: 0,
-            y: 30
-        });
-        gsap.set(".item.i15 .item__inner", {
-            opacity: 0,
-            y: 30
-        });
-        gsap.set(".item.i4 .item__inner", {
-            opacity: 0,
-            y: 30
-        });
-        gsap.set(".item.i3 .item__inner", {
-            opacity: 0,
-            y: 30
-        });
-        gsap.set(".item.i14 .item__inner", {
-            opacity: 0,
-            y: 30
-        });
-
-
-
-
-
-        gsap.to(".item.i1 .item__bg", {
-            height: "0%",
-            ease: "power3.inOut",
-            delay: 0.3
-        });
-        gsap.to(".item.i5 .item__bg", {
-            height: "0%",
-            ease: "power3.inOut",
-            overflowY: "hidden", // Corrected property name
-            delay: 0.6
-        });
-        gsap.to(".item.i7 .item__bg", {
-            height: "0%",
-            ease: "power3.inOut",
-            delay: 0.9
-        })
-        gsap.to(".item.i15 .item__bg", {
-            height: "0%",
-            ease: "power3.inOut",
-            delay: 1.2
-        })
-        gsap.to(".item.i4 .item__bg", {
-            height: "0%",
-            ease: "power3.inOut",
-            delay: 1.5
-        });
-        gsap.to(".item.i3 .item__bg", {
-            height: "0%",
-            ease: "power3.inOut",
-            delay: 1.5
-        })
-        gsap.to(".item.i14 .item__bg", {
-            height: "0%",
-            ease: "power3.inOut",
-            delay: 1.7
-        })
-        // gsap.to(".item.i16 .item__bg", {
-        //     height: "0%",
-        //     ease: "power3.inOut",
-        //     delay: 1.7
-        // })
-        // gsap.to(".item.i6 .item__bg", {
-        //     height: "0%",
-        //     ease: "power3.inOut",
-        //     delay: 1.9
-        // })
-        // gsap.to(".item.i10 .item__bg", {
-        //     height: "0%",
-        //     ease: "power3.inOut",
-        //     delay: 1.9
-        // })
-        // gsap.to(".item.i13 .item__bg", {
-        //     height: "0%",
-        //     ease: "power3.inOut",
-        //     delay: 1.9
-        // })
-        // gsap.to(".item.i2 .item__bg", {
-        //     height: "0%",
-        //     ease: "power3.inOut",
-        //     delay: 2.1
-        // })
-        // gsap.to(".item.i4 .item__bg", {
-        //     height: "0%",
-        //     ease: "power3.inOut",
-        //     delay: 2.1
-        // })
-        // gsap.to(".item.i5 .item__bg", {
-        //     height: "0%",
-        //     ease: "power3.inOut",
-        //     delay: 2.1
-        // })
-
-
-
-        gsap.to(".item.i1 .item__inner", {
-            opacity: 1,
-            delay: 0.6,
-            y: 0
-        });
-        gsap.to(".item.i5 .item__inner", {
-            opacity: 1,
-            delay: 0.9,
-            y: 0
-        });
-        gsap.to(".item.i7 .item__inner", {
-            opacity: 1,
-            delay: 1.2,
-            y: 0
-        });
-        gsap.to(".item.i15 .item__inner", {
-            opacity: 1,
-            delay: 1.5,
-            y: 0
-        });
-        gsap.to(".item.i4 .item__inner", {
-            opacity: 1,
-            delay: 1.8,
-            y: 0
-        });
-        gsap.to(".item.i3 .item__inner", {
-            opacity: 1,
-            delay: 1.8,
-            y: 0
-        });
-        gsap.to(".item.i14 .item__inner", {
-            opacity: 1,
-            delay: 2.1,
-            y: 0
-        });
-
-
-        // gsap를 작동하지 않도록 할 링크 선택자를 배열로 저장
-        const excludedLinks = [
-            "#item__wrap4 .item.i3 a:nth-of-type(2)",
-            "#item__wrap4 .item.i3 a:nth-of-type(3)",
-            "#item__wrap4 .item.i3 a:nth-of-type(4)",
-        ];
-
-        const linkClickHandler = (linkSelector, destination, 새창여부 = false) => {
-            const linkElement = document.querySelector(linkSelector);
-
-            // gsap를 작동하지 않도록 할 링크인지 확인
-            const isExcluded = excludedLinks.includes(linkSelector);
-
-            linkElement.addEventListener('click', (event) => {
-                event.preventDefault(); // 링크의 기본 동작 방지
-
-                if (!isExcluded) {
-                    gsap.to(".item__bg", {
-                        height: "100%",
-                        ease: "power3.inOut",
-                        onComplete: () => {
-                            // 애니메이션이 완료된 후 링크로 이동
-                            if (새창여부) {
-                                window.open(destination, '_blank'); // 새 창에서 링크 열기
-                            } else {
-                                window.location.href = destination; // 현재 창에서 링크 열기
-                            }
-                        },
-                    });
-                } else {
-                    // gsap를 작동하지 않도록 할 링크일 경우 바로 링크 이동
-                    if (새창여부) {
-                        window.open(destination, '_blank');
-                    } else {
-                        window.location.href = destination;
-                    }
-                }
+        const setInitialAnimationStates = (selectors) => {
+            selectors.forEach((selector) => {
+                gsap.set(selector, {
+                    opacity: 0,
+                    y: 30,
+                });
             });
         };
 
-        // ...
+        setInitialAnimationStates([
+            '.item.i1 .item__inner',
+            '.item.i5 .item__inner',
+            '.item.i7 .item__inner',
+            '.item.i15 .item__inner',
+            '.item.i4 .item__inner',
+            '.item.i3 .item__inner',
+            '.item.i14 .item__inner',
+        ]);
 
-        linkClickHandler("#item__wrap4 .item.i4", "/");
-        linkClickHandler("#item__wrap4 .item.i3 a:nth-of-type(1)", "/contact");
-        linkClickHandler("#item__wrap4 .item.i3 a:nth-of-type(2)", "https://github.com/jinhomun", true);
-        linkClickHandler("#item__wrap4 .item.i3 a:nth-of-type(3)", "mailto:answlsgh95@gmail.com", true);
-        linkClickHandler("#item__wrap4 .item.i3 a:nth-of-type(4)", "https://www.instagram.com/coding_jinho/", true);
+        gsap.to('.item.i1 .item__bg', {
+            height: '0%',
+            ease: 'power3.inOut',
+            delay: 0.3,
+        });
 
+        gsap.to('.item.i5 .item__bg', {
+            height: '0%',
+            ease: 'power3.inOut',
+            overflowY: 'hidden',
+            delay: 0.6,
+        });
 
+        gsap.to('.item.i7 .item__bg', {
+            height: '0%',
+            ease: 'power3.inOut',
+            delay: 0.9,
+        });
+
+        gsap.to('.item.i15 .item__bg', {
+            height: '0%',
+            ease: 'power3.inOut',
+            delay: 1.2,
+        });
+
+        gsap.to('.item.i4 .item__bg', {
+            height: '0%',
+            ease: 'power3.inOut',
+            delay: 1.5,
+        });
+
+        gsap.to('.item.i3 .item__bg', {
+            height: '0%',
+            ease: 'power3.inOut',
+            delay: 1.5,
+        });
+
+        gsap.to('.item.i14 .item__bg', {
+            height: '0%',
+            ease: 'power3.inOut',
+            delay: 1.7,
+        });
+
+        const animateInnerElements = (selectors, delay) => {
+            selectors.forEach((selector, index) => {
+                gsap.to(selector, {
+                    opacity: 1,
+                    delay: delay + index * 0.3,
+                    y: 0,
+                });
+            });
+        };
+
+        animateInnerElements(
+            [
+                '.item.i1 .item__inner',
+                '.item.i5 .item__inner',
+                '.item.i7 .item__inner',
+                '.item.i15 .item__inner',
+                '.item.i4 .item__inner',
+                '.item.i3 .item__inner',
+                '.item.i14 .item__inner',
+            ],
+            0.6
+        );
+
+        window.addEventListener('unload', handleUnload);
+
+        return () => {
+            window.removeEventListener('unload', handleUnload);
+        };
     }, []);
+
+    const handleUnload = () => {
+        gsap.killTweensOf('.item__bg');
+    };
+
+    const excludedLinks = [
+        '#item__wrap4 .item.i3 a:nth-of-type(2)',
+        '#item__wrap4 .item.i3 a:nth-of-type(3)',
+        '#item__wrap4 .item.i3 a:nth-of-type(4)',
+    ];
+
+    const linkClickHandler = (linkSelector, destination, newTab = false) => {
+        const linkElement = document.querySelector(linkSelector);
+        const isExcluded = excludedLinks.includes(linkSelector);
+
+        linkElement.addEventListener('click', (event) => {
+            event.preventDefault();
+
+            if (!isExcluded) {
+                gsap.to('.item__bg', {
+                    height: '100%',
+                    ease: 'power3.inOut',
+                    onComplete: () => {
+                        if (newTab) {
+                            window.open(destination, '_blank');
+                        } else {
+                            window.location.href = destination;
+                        }
+                    },
+                });
+            } else {
+                if (newTab) {
+                    window.open(destination, '_blank');
+                } else {
+                    window.location.href = destination;
+                }
+            }
+        });
+    };
+
+    linkClickHandler('#item__wrap4 .item.i4', '/');
+    linkClickHandler('#item__wrap4 .item.i3 a:nth-of-type(1)', '/contact');
+    linkClickHandler('#item__wrap4 .item.i3 a:nth-of-type(2)', 'https://github.com/jinhomun', true);
+    linkClickHandler('#item__wrap4 .item.i3 a:nth-of-type(3)', 'mailto:answlsgh95@gmail.com', true);
+    linkClickHandler('#item__wrap4 .item.i3 a:nth-of-type(4)', 'https://www.instagram.com/coding_jinho/', true);
+
+
     return (
         <>
             <div id="item__wrap4" className="fixed">
